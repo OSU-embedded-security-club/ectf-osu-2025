1cb8ae9b7bc073974714f40b1ff076c3f18a1843
Mark Bundschuh <mark@mbund.dev>
2024-11-19 01:26:28 -0500

    improve ci build times

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    msdk = { url = "github:Analog-Devices-MSDK/msdk/v2023_06"; flake = false; };
+    msdk = {
+      url = "github:Analog-Devices-MSDK/msdk/v2023_06?shallow=1";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -18,25 +21,26 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs = [
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
               zig
               zls
               clang-tools
               bear
+              alejandra
 
               gnumake
               python39
               gcc-arm-embedded
               poetry
               cacert
-              (pkgs.callPackage pkgs/analog_openocd.nix { })
+              (pkgs.callPackage pkgs/analog_openocd.nix {})
               minicom
             ];
 
-            GCC_ARM_EMBDEDDED = gcc-arm-embedded;
+            GCC_ARM_EMBDEDDED = pkgs.gcc-arm-embedded;
 
             shellHook = ''
               cp -r ${msdk} $PWD/msdk
@@ -44,6 +48,25 @@
               export MAXIM_PATH=$PWD/msdk
             '';
           };
-        }
+
+          ectf = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              zig
+              gnumake
+              python39
+              gcc-arm-embedded
+              poetry
+            ];
+
+            GCC_ARM_EMBDEDDED = pkgs.gcc-arm-embedded;
+
+            shellHook = ''
+              cp -r ${msdk} $PWD/msdk
+              chmod -R u+rwX,go+rX,go-w $PWD/msdk
+              export MAXIM_PATH=$PWD/msdk
+            '';
+          };
+        };
+      }
     );
 }
--- shell.nix
+++ shell.nix
@@ -11,4 +11,7 @@
   )
   {src = ./.;}
 )
-.shellNix
+.defaultNix
+.devShells
+."${builtins.currentSystem}"
+.ectf
