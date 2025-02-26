37f75495e3944cb078951aa53b0e9d1bb9776d57
Christopher McDevitt <47703105+RebelAndroid@users.noreply.github.com>
2025-02-26 15:26:41 -0500

    Update hardware.zig
    Signed-off-by: Christopher McDevitt <47703105+RebelAndroid@users.noreply.github.com>

--- decoder/src/hardware.zig
+++ decoder/src/hardware.zig
@@ -7,23 +7,23 @@ const msdk = @import("msdk");
 pub fn mpu() void {
     msdk.ARM_MPU_Disable();
 
-    // make SRAM non-executable
+    // Make SRAM non-executable
     msdk.ARM_MPU_SetRegion(
-        msdk.ARM_MPU_RBAR(1, 0x20000000),
+        msdk.ARM_MPU_RBAR(0, 0x20000000),
         msdk.ARM_MPU_RASR(1, msdk.ARM_MPU_AP_FULL, msdk.ARM_MPU_ACCESS_ORDERED, 0, 0, 0, 0b00000000, msdk.ARM_MPU_REGION_SIZE_1MB),
     );
     msdk.ARM_MPU_Enable(msdk.MPU_CTRL_HFNMIENA_Msk | msdk.MPU_CTRL_PRIVDEFENA_Msk);
 
-    // except for this region which must be executable
+    // Except for this region which must be executable
     msdk.ARM_MPU_SetRegion(
-        msdk.ARM_MPU_RBAR(2, 0x20000000),
+        msdk.ARM_MPU_RBAR(1, 0x20000000),
         msdk.ARM_MPU_RASR(0, msdk.ARM_MPU_AP_FULL, msdk.ARM_MPU_ACCESS_ORDERED, 0, 0, 0, 0b10000000, msdk.ARM_MPU_REGION_SIZE_4KB),
     );
     msdk.ARM_MPU_Enable(msdk.MPU_CTRL_HFNMIENA_Msk | msdk.MPU_CTRL_PRIVDEFENA_Msk);
 
     // Make FLASH read only
     msdk.ARM_MPU_SetRegion(
-        msdk.ARM_MPU_RBAR(3, 0x10000000),
+        msdk.ARM_MPU_RBAR(2, 0x10000000),
         msdk.ARM_MPU_RASR(0, msdk.ARM_MPU_AP_RO, msdk.ARM_MPU_ACCESS_ORDERED, 0, 0, 0, 0b00000000, msdk.ARM_MPU_REGION_SIZE_256KB),
     );
 
