82f259bfd44ebad40ec1ea007f61907c97722d0d
Joshua Sims <jm8hjkl@gmail.com>
2025-02-26 15:42:13 -0500

    Code to modify it

--- logtest/main2.py
+++ logtest/main2.py
@@ -14,7 +14,8 @@ def main():
     parser.add_argument("secrets")
     parser.add_argument("port")
     args = parser.parse_args()
-    data = ast.literal_eval(Path(args.data).read_text())
+    data = json.loads(Path(args.data).read_text())
+
     secrets = Path(args.secrets).read_bytes()
     print(secrets)
     port = args.port
@@ -25,9 +26,13 @@ def main():
     device_id = 0xDEADBEEF
     bad_device_id = 0xCAFEBABE
 
+    curr_timestamp = 0
+    output = {}
     for name, commands in data.items():
         print(f"\n\n\n\n======== RUNNING {name} ============\n\n\n\n")
+        outcommands = []
         for command in commands:
+            should_error = False
             try:
                 op, *args = command.split()
                 if op == "subscribe" or op == "bad_subscribe":
@@ -46,27 +51,37 @@ def main():
                     except Exception as e:
                         print("CAUGHT ENCODER ERROR")
                         subscription = f"ERROR: Encoder exception: {e}".encode()
-                    decoder.subscribe(subscription)
+                        should_error = True
+                    # decoder.subscribe(subscription)
                 elif op == "list":
                     decoder.list()
                 elif op == "decode":
                     channel, timestamp, *frame = args
+                    if int(timestamp) < curr_timestamp:
+                        should_error = True
+                    curr_timestamp = int(timestamp)
                     frame = ast.literal_eval(" ".join(frame)).encode()
                     try:
                         encoded = encoder.encode(int(channel), frame, int(timestamp))
                     except Exception as e:
                         print("CAUGHT ENCODER ERROR")
                         encoded = f"ERROR: Encoder exception: {e}".encode()
-                    decoder.decode(encoded)
+                        should_error = True
+                    # decoder.decode(encoded)
                 elif op == "flash":
-                    input("Please flash and then press enter")
+                    pass
+                    # input("Please flash and then press enter")
                 elif op == "power_cycle":
-                    input("Please power cycle and then press enter")
-                else:
-                    raise ValueError(op)
+                    curr_timestamp = 0
+                    # input("Please power cycle and then press enter")
+                    pass
+                outcommands.append({"command": command, "expected": not should_error})
             except Exception as e:
                 traceback.print_exception(e)
-                input(f"Caught error in {name}, press enter to continue")
+                # input(f"Caught error in {name}, press enter to continue")
+                pass
+        output[name] = outcommands
+    Path("data.modified.json").write_text(json.dumps(output, indent=2))
 
 
 if __name__ == "__main__":
